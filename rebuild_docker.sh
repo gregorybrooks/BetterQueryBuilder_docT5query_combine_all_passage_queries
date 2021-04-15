48083066144d3fffff1624c7ab17f3a332c71493
Greg Brooks <gregorybrooks@gmail.com>
2021-04-15 10:29:22 -0400

    Make Docker image not qualified

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -2,4 +2,3 @@ set -v
 # NOTE: Docker image names must be all lower-case
 docker rmi doct5query_combine_all_passage_queries
 docker build -t doct5query_combine_all_passage_queries .
-docker tag doct5query_combine_all_passage_queries gregorybrooks/doct5query_combine_all_passage_queries
